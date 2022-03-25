69cae9fd0f1343689235d6358238d54105d4d104
Anna Varaksina <enigmasnape@gmail.com>
2022-03-25 19:30:48 +0300

    Правки

--- src/app/pipes/rate.pipe.ts
+++ src/app/pipes/rate.pipe.ts
@@ -5,11 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class RatePipe implements PipeTransform {
 
-  public transform(rating: number): number | undefined {
-		const ratingAfterDot = rating % 1;
-		if (ratingAfterDot < 0.25) {
-			return Math.trunc(rating);
-		} 
+  public transform(rating: number | undefined): number | undefined {
+
+	if (typeof rating === 'undefined') {
+		return;
+	}
+	const ratingAfterDot = rating % 1;
+
+	if (ratingAfterDot < 0.25) {
+		return Math.trunc(rating);
+	} 
+
     if (ratingAfterDot >= 0.25 && ratingAfterDot < 0.75) {
 			return Math.trunc(rating) + 0.5;
 		} 
